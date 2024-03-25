eec7a344c1fb906a2b26288baa184ebb2cfd66d9
Nimra <161188938+Nimra6652@users.noreply.github.com>
2024-03-26 03:57:22 +0500

    created number gessing game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\Welcome To \'CodeWithNimra\' -CLI Number Guessing Game\n");
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessdNumber",
+        type: "number",
+        message: "please guess a number between 1-6: ",
+    },
+]);
+if (answers.userGuessdNumber === randomNumber) {
+    console.log("Congratulations! you gussed a right number.");
+}
+else {
+    console.log("Sorry! you guessed a wrong number.");
+}
